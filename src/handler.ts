0f8532f9c4cecc117482b9d62d73c7e629b50a1e
Gadi Piperno Corcos <gadi@skyriseltd.com>
2021-03-08 18:32:14 +0100

    :recycle: Removed all logic to use cloudflare just to put get matchData

--- src/handler.ts
+++ src/handler.ts
@@ -1,20 +1,6 @@
 declare const MATCHES: KVNamespace
 
-declare global {
-  interface WorkerGlobalScope {
-    matches: Record<string,any>
-  }
-}
-
-import * as jsondiffpatch from 'jsondiffpatch'
 import { getResponse200, getResponse404 } from './utils'
-
-const jsondiff = jsondiffpatch.create()
-
-if(!self.matches){
-  self.matches = {}
-}
-
 
 export async function handleRequest(request: Request, event: FetchEvent): Promise<Response> {
   let url = new URL(request.url)
@@ -42,38 +28,8 @@ export async function handleRequest(request: Request, event: FetchEvent): Promis
     return getResponse404()
   })
 }
-
-function getStateId(state: any){
-  return state && state.id
-}
-
 async function updateMatch(data: any){
-  let { match_id, map, players, state } = data || {}
+  let { match_id } = data || {}
   if(!match_id) return
-
-  let match = await MATCHES.get(match_id, 'json') as any
-
-  let memoryMatch = self.matches[match_id]
-
-  if (memoryMatch){
-    let memoryStateId = getStateId(memoryMatch.state)
-    let diskStateId = getStateId(match && match.state) 
-
-    if (memoryStateId >= diskStateId) {
-      match = memoryMatch
-    }
-  }
-
-  match = Object.assign({ state, deltas: [], map }, match, { players })
-
-
-  if (getStateId(state) > getStateId(match.state)){
-    let delta = jsondiff.diff(match.state, state)
-    match.deltas.push(delta)
-    match.state = state
-  }
-
-  self.matches[match_id] = match
-
-  return MATCHES.put(match_id, JSON.stringify(match))
+  return MATCHES.put(match_id, JSON.stringify(data))
 }
